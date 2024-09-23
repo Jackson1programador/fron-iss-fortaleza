8c9f20e087b63980b365061b47ddc046fbfbfa92
Jackson1programador <jackson1contador.gmail.com>
2024-09-23 08:23:42 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//component
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ErroComponent } from './pages/erro/erro.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent, pathMatch: "full"},
+  {path: "login", component: LoginComponent},
+  {path: "cadastro", component: CadastroComponent},
+  {path: "404", component: ErroComponent},
+  {path: "**", redirectTo: '404' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ErroComponent } from './pages/erro/erro.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { RodapeComponent } from './shared/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    LoginComponent,
+    HomeComponent,
+    ErroComponent,
+    CadastroComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
